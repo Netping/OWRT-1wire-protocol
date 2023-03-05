de0d3507ad3f9d6f9eaa443e80ff67011be63dd7
Aleksandr Shenshin <aashenshin@yandex.ru>
2023-03-06 00:33:51 +0300

    [src] Add search 1wire device

--- etc/owrt-1-wire-protocol.py
+++ etc/owrt-1-wire-protocol.py
@@ -19,23 +19,42 @@ def stop_run(signum, frame):
 signal.signal(signal.SIGTERM, stop_run)
 
 
+def get_list_1wire(dir_mnt):
+    dev_1wire = []
+    ret_val = 0
+    try:
+        with os.scandir(dir_mnt) as files:
+            subdir = [file.name for file in files if file.is_dir()]
+            for i in range(len(subdir)):
+                dir_name = subdir[i].split(".")
+                if len(dir_name) == 2:
+                    try:
+                        int(dir_name[0], 16)
+                        int(dir_name[1], 16)
+                        dev_1wire.append(subdir[i])
+                    except ValueError:
+                        pass
+    except FileNotFoundError:
+        journal.WriteLog("OWRT-1-wire-protocol", "Normal", "err", "Not found mount dir OWFS")
+        ret_val = 1
+
+    return ret_val, dev_1wire
+
+
 if __name__ == '__main__':
 
     try:
         result = subprocess.run(["owfs", "--fake=10,22", "-m", dir_owfs], check=True)
-    except subprocess.CalledProcessError:
+    except (FileNotFoundError, subprocess.CalledProcessError):
         journal.WriteLog("OWRT-1-wire-protocol", "Normal", "err", "Failed run OWFS")
         sys.exit(-1)
 
     journal.WriteLog("OWRT-1-wire-protocol", "Normal", "notice", "Start module!")
 
-#    list_dir = os.listdir(dir_owfs)
-#    for name_dir in list_dir:
-#        if os.path.isdir(name_dir):
-#            print(name_dir)
-    with os.scandir(dir_owfs) as files:
-        subdir = [file.name for file in files if file.is_dir()]
-    print(subdir)
+    ret_val, list_1wire = get_list_1wire(dir_owfs)
+    if ret_val:
+        fl_run_main = False
+    print(list_1wire)
 
     try:
         while fl_run_main:
